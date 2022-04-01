c5a1b2ba4e94d0bd441ce40e3deba7459ee335a3
GersonBenito <gersonbenito4@gmail.com>
2022-04-01 08:41:40 -0600

    feat(connect.js) se modifico a una promesa la funcion de conexion a la base de datos de mongodb

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,15 +1,20 @@
 const { connect } = require('mongoose');
 
-const dbConnect = async() =>{
-    try {
-        await connect(process.env.DB_CONNECTION, {
-            useNewUrlParser: true,
-        });
-
-        console.log('connected to database of mongodb');
-    } catch (error) {
-        console.log('ocurred a error to try connected to database', error);
-    }
+const dbConnect = () => {
+    return new Promise((resolve, reject)=>{
+        console.log('connecting to database mongodb...');
+        try {
+            connect(process.env.DB_CONNECTION, {
+                useNewUrlParser: true,
+            });
+            resolve(true);
+            console.log('connected to database of mongodb');
+        } catch (error) {
+            reject(false);
+            console.log('ocurred a error to try connected to database', error);
+        }
+    });
 }
+
 
 module.exports = { dbConnect }
