75c87a6edc01a23d681d029a217bd9124fcf3476
GersonBenito <gersonbenito4@gmail.com>
2022-06-26 15:44:21 -0600

    feat(varius) new endpoint added, get quiz by code

--- src/controllers/quizz.controller.js
+++ src/controllers/quizz.controller.js
@@ -105,10 +105,34 @@ const getQuizzById = async (req, res = response) =>{
     }
 }
 
+const getQuizByCode = async (req, res = response) =>{
+    try {
+        const code = req.params.code;
+        const quiz = await Quizz.findOne({code: code});
+        if(!quiz){
+            return res.status(404).json({
+                status: 404,
+                message: 'Invalid code'
+            });
+        }
+        return res.status(200).json({
+            status: 200,
+            data: quiz,
+            message: 'Quiz by code get success'
+        });
+    } catch (error) {
+        return res.status(400).json({
+            status: 400,
+            message: 'Error to get quiz by code'
+        });
+    }
+}
+
 module.exports = {
     createQuizz,
     getAllQuizz,
     getQuizzByIdUser,
     deleteQuizz,
     getQuizzById,
+    getQuizByCode,
 }
--- src/routes/quizz.router.js
+++ src/routes/quizz.router.js
@@ -6,6 +6,7 @@ const quizz = require('../controllers/quizz.controller');
 router.get('/', quizz.getAllQuizz);
 router.get('/:id', quizz.getQuizzByIdUser);
 router.get('/details/:id', quizz.getQuizzById);
+router.get('/search/:code', quizz.getQuizByCode);
 router.delete('/:id', quizz.deleteQuizz);
 router.post('/', quizz.createQuizz);
 
